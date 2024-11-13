77e2f28d6e3a9d7490212440d48e4e7714780d84
Daniel Ribeiro <91394588+danielribeiro1@users.noreply.github.com>
2024-11-13 15:18:57 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'inference = face_recognition.yolo_inference:main'
+            'inference = face_recognition.teste_final:main'
         ],
     },
 )
